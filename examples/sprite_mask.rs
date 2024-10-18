1322aab1419c1380bdd8078667b21968f3c54135
Elase Huang <elase.huang@outlook.com>
2024-10-19 03:29:34 +0800

    refactor

--- examples/sprite_mask.rs
+++ examples/sprite_mask.rs
@@ -42,8 +42,19 @@ fn setup_sprites(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..default()
     });
     // sprite mask 只会应用在第一个 sprite 上，而不会应用在第二个 sprite 上
+    // 同时因为现在只支持一个 mask，所以 mask02 会生效而 mask01 不会生效
     commands.spawn(SpriteMaskBundle {
-        texture: asset_server.load("sprite02.png"),
+        texture: asset_server.load("mask01.png"),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        sprite_mask: SpriteMask {
+            range_start: 0,
+            range_end: 5,
+            ..default()
+        },
+        ..default()
+    });
+    commands.spawn(SpriteMaskBundle {
+        texture: asset_server.load("mask02.png"),
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
         sprite_mask: SpriteMask {
             range_start: 0,
